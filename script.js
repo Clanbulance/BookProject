ccc55eec3745e20757a082e485455f055c5dc867
Robbert-jan <robbertjan007@hotmail.com>
2024-01-26 16:46:05 +0100

    added a button and visuals

--- script.js
+++ script.js
@@ -7,6 +7,8 @@
 // add a delete button to the books
 
 // .prototype
+const contentPlace = document.querySelector(".content");
+
 function book(title,author,pages,read){
     this.title = title;
     this.author = author;
@@ -30,6 +32,7 @@ const createBooks = function(array){
 array.forEach(book = function(book){
 let newBook=  document.createElement("div");
 newBook.classList.add('book');
+
 newBook.innerHTML = `
 <div class="title">Title:</div>
 <div class="titleValue">${book.title}</div>
@@ -39,11 +42,39 @@ newBook.innerHTML = `
 <div class="pagesValue">${book.pages}</div>
 <div class="read">Read:</div>
 <div class="readValue">${book.read}</div>
-<input type="button" class="button" onclick="removeItem">
-
+<input type="button" class="button" onclick="removeItem(${myLibrary.indexOf(book)})" id=${myLibrary.indexOf(book)}} value="Remove book">
 `
+
 workArea.appendChild(newBook);
 })}
+
+function removeItem(e){
+    console.log(`Deleting ${e} from array`);
+    delete myLibrary[e];
+    contentPlace.innerHTML = `
+    <div class="book" id="AddButton" onclick=createNewBook()>Click to Add a book</div>
+    `
+    console.log(`Array is now ${myLibrary}`)
+    createBooks(myLibrary)
+}
+
+let createNewBook = function(){
+    console.log("Lets get a new book");
+    document.querySelector(".hiddenPopUp").style.display = "flex"
+    contentPlace.style.opacity = "50%"
+}
+
+let closeMenu = function(){
+    document.querySelector(".hiddenPopUp").style.display = "none";
+    contentPlace.style.opacity = "100%"
+}
+
+document.getElementById('submit-btn').addEventListener('click',
+    function(event){
+        event.preventDefault();
+        console.log("submitted")
+        closeMenu();
+    })
 
 
 
