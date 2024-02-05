944fd8bb8f6c374a8b5ee8f349616e71ee6071e0
Robbert-jan <robbertjan007@hotmail.com>
2024-02-05 17:06:28 +0100

    finished exc visuals

--- script.js
+++ script.js
@@ -9,40 +9,43 @@
 // .prototype
 const contentPlace = document.querySelector(".content");
 
-function book(title,author,pages,read){
+const book = function(title,author,pages,read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
 
-const book1 = new book("Thinking fast or thinking slow","Daniel Kahneman","643","notread");
+const book1 = new book("Thinking fast or thinking slow","Daniel Kahneman","643","not finished");
 const book2 = new book("wrwrwrwr","JK.Bowling","543","read");
 
 let myLibrary = [book1,book2];
 
-myLibrary.forEach(book = function(book){
-    console.log(`${book.title} has been created`)
+myLibrary.forEach((elem) => {
+    console.log(`${elem.title} has been created`)
 })
 
 const workArea = document.querySelector(".content");
 
 // This function is creating all books from the array
 const createBooks = function(array){
-array.forEach(book = function(book){
+    contentPlace.innerHTML = `
+    <div class="book" id="AddButton" onclick=createNewBook()>Click to Add a book</div>
+    `
+array.forEach((elem) =>{
 let newBook=  document.createElement("div");
 newBook.classList.add('book');
 
 newBook.innerHTML = `
 <div class="title">Title:</div>
-<div class="titleValue">${book.title}</div>
+<div class="titleValue">${elem.title}</div>
 <div class="author">Author:</div>
-<div class="authorValue">${book.author}</div>
+<div class="authorValue">${elem.author}</div>
 <div class="pages">Pages:</div>
-<div class="pagesValue">${book.pages}</div>
+<div class="pagesValue">${elem.pages}</div>
 <div class="read">Read:</div>
-<div class="readValue">${book.read}</div>
-<input type="button" class="button" onclick="removeItem(${myLibrary.indexOf(book)})" id=${myLibrary.indexOf(book)}} value="Remove book">
+<div class="readValue">${elem.read}</div>
+<input type="button" class="button" onclick="removeItem(${myLibrary.indexOf(elem)})" id=${myLibrary.indexOf(elem)}} value="Remove book">
 `
 
 workArea.appendChild(newBook);
@@ -69,14 +72,40 @@ let closeMenu = function(){
     contentPlace.style.opacity = "100%"
 }
 
+
 document.getElementById('submit-btn').addEventListener('click',
     function(event){
         event.preventDefault();
-        console.log("submitted")
+        console.log("submitted");
+        let title = document.getElementById("titleInput").value; 
+        let author = document.getElementById("authorInput").value; 
+        let pages= document.getElementById("numberInput").value; 
+        let read = document.getElementById("checkboxInput").value; 
+        let newListing = new book(title,author,pages,read);
+        console.log(newListing);
+        myLibrary.push(newListing);
+        console.log(myLibrary)
+        createBooks(myLibrary);
         closeMenu();
+        clearInputFields();
     })
 
+const clearInputFields = function(){
+    document.getElementById("titleInput").value = ""; 
+    document.getElementById("authorInput").value = "";
+    document.getElementById("numberInput").value = "";
+    document.getElementById("checkboxInput").value = "";
+}
 
+
+let setValue = function(){
+    let chkbx =  document.getElementById("checkboxInput");
+    if (chkbx.checked == true){
+        chkbx.value = "read"
+    } else {
+        chkbx.value = "not finished"
+    }
+}
 
 createBooks(myLibrary);
 
